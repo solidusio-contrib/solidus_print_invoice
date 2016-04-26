93e816a2eb15362b05424e207ff73f4981208f07
Richard Wilson <richard@freerunningtech.com>
2016-04-26 16:50:53 -0700

    Verify pdf generation works and is properly sent to the client.
    We shouldn't really test the content of the PDF here. We'd essentially
    be testing Prawn, and while view specs are something we can do we don't
    normally go through the effort.
    
    Here we're just verifying that the PDF can be generated without error
    and with the correct status code and headers.

---
+++ spec/requests/admin_orders_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe "Order Invoice Generation" do
+  describe "GET show" do
+    let(:order) { FactoryGirl.create(:completed_order_with_totals) }
+    let(:user) { FactoryGirl.create(:admin_user, password: "testing123") }
+    let(:headers) {{ "X-Spree-Token" => user.spree_api_key }}
+
+    it "returns a pdf" do
+      # The invoice generation feature is restricted to admins
+      post(spree.login_path, 'spree_user[email]' => user.email, 'spree_user[password]' => 'testing123')
+      get(spree.admin_order_path(order.number, :pdf))
+
+      aggregate_failures "pdf response is successful" do
+        expect(response).to be_success
+        expect(response.headers["Content-Type"]).to eql("application/pdf; charset=utf-8")
+        expect(response.body).to start_with('%PDF')
+      end
+    end
+  end
+end
