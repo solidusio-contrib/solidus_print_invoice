7478624f696a462a5a377b6c029fbc1de6a5759e
John Hawthorn <john@stembolt.com>
2017-02-07 15:33:59 -0800

    Remove configuration from admin
    Fixes #4

--- app/overrides/admin_invoices_settings.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
-                     :name => "admin_print_invoice_settings",
-                     :insert_before => ".filter-actions.form-buttons",
-                     :partial => "spree/admin/general_settings/invoice",
-                     # :disabled => false)
-                     :disabled => !Object.const_defined?('SolidusPrintInvoice'))
-
-
