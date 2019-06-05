ddeaf4108e918f3be37e33d4d16f133aa735383f
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-06-05 12:58:27 -0500

    Load SolidusPrintInvoice configuration file

--- lib/solidus_print_invoice.rb
+++ lib/solidus_print_invoice.rb
@@ -1,12 +1,5 @@
 require 'solidus_core'
 require 'solidus_support'
+require 'solidus_print_invoice/configuration'
 require 'solidus_print_invoice/engine'
 require 'prawn_handler'
-
-module Spree
-  module PrintInvoice
-    def self.config(&block)
-      yield(Spree::PrintInvoice::Config)
-    end
-  end
-end
--- lib/solidus_print_invoice/engine.rb
+++ lib/solidus_print_invoice/engine.rb
@@ -2,26 +2,29 @@ require 'deface'
 
 module SolidusPrintInvoice
   class Engine < Rails::Engine
+    isolate_namespace Spree
     engine_name 'solidus_print_invoice'
-    
-    initializer "spree.print_invoice.environment", :before => :load_config_initializers do |app|
-      Spree::PrintInvoice::Config = Spree::PrintInvoiceConfiguration.new
-    end
-    
-    initializer "spree.print_invoice.mimetypes" do |app|
-      Mime::Type.register('application/pdf', :pdf) unless Mime::Type.lookup_by_extension(:pdf)
-    end
-    
-    def self.activate
-
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-
-    end
 
     config.autoload_paths += %W(#{config.root}/lib)
-    config.to_prepare &method(:activate).to_proc
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    initializer 'solidus_print_invoice.environment', before: :load_config_initializers do |app|
+      SolidusPrintInvoice::Config = SolidusPrintInvoice::Configuration.new
+    end
+
+    initializer 'solidus_print_invoice.mimetypes' do |app|
+      Mime::Type.register('application/pdf', :pdf) unless Mime::Type.lookup_by_extension(:pdf)
+    end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
